91a297b0b77aa4c32203e9cf964a13883871b000
Hoang Vu <tranhoangvu2525@gmail.com>
2025-04-13 00:35:08 +0700

    Sort order history by createdAt descending

--- controllers/adminHistoryController.js
+++ controllers/adminHistoryController.js
@@ -3,7 +3,7 @@ const Order = require("../models/Order");
 // Lấy toàn bộ danh sách đơn hàng
 exports.getAllHistory = async (req, res) => {
   try {
-    const histories = await Order.find();
+    const histories = await Order.find().sort({ createdAt: -1 });
     res.status(200).json(histories);
   } catch (error) {
     res.status(500).json({ message: "Lỗi khi lấy dữ liệu", error });
